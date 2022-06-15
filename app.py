3b6ab311aa31824d18699637092f243bac4688fe
anteCedens <jaksa.juric@yahoo.com.hr>
2022-06-15 00:50:58 +0000

    Set up "Hello World" message to test Flask install

--- app.py
+++ app.py
@@ -2,3 +2,18 @@ import os
 from flask import Flask
 if os.path.exists('env.py'):
     import env
+
+
+# Create an instance of Flask, and store it in a variable called "app"
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
