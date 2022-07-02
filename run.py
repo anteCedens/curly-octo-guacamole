3c39e36610c2571947bb8b50f257b6e797ee7bf6
anteCedens <jaksa.juric@yahoo.com.hr>
2022-07-02 00:31:47 +0000

    Change run.py to use render_template
    - set render_template import
    - comment out references to env.py
    - set up default route to return index.html via render_template
    - create templates/index.html
    - add test content to index.html
    - set default fallbacks to IP and PORT in run.py
    - add comments to run.py

--- run.py
+++ run.py
@@ -1,19 +1,22 @@
 import os
-from flask import Flask
-if os.path.exists('env.py'):
-    import env
+from flask import Flask, render_template
+#if os.path.exists('env.py'):
+#    import env
 
 
 # Create an instance of Flask, and store it in a variable called "app"
 app = Flask(__name__)
 
+# This url ("/") should trigger the function that follows ("hello")
+
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 
+# If that is the name, then run the app with the following arguments
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP', '0.0.0.0'),  # Get IP if it exists, if not use default (0.0.0.0)
+            port=int(os.environ.get('PORT', '5000')),
             debug=True)
