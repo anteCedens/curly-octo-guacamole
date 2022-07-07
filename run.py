bcacfcde1119aa797be5b7aec927a7e534034b9f
anteCedens <jaksa.juric@yahoo.com.hr>
2022-07-07 18:06:47 +0000

    Set a flashed message for the 'contact' route

--- run.py
+++ run.py
@@ -38,6 +38,9 @@ def about_member(member_name):
 # Any method allowed beyond POST needs to be explicitly stated
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == 'POST':
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
